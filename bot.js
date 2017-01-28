16509624a082c5adf7259ff08cbc63b4974cb677
Rick Budzak <pyroja@gmail.com>
2017-01-27 23:25:17 -0800

    can follow specific users now

--- bot.js
+++ bot.js
@@ -1,6 +1,22 @@
 const Twit = require('twit');
-var config = require('./config')
+var config = require('./config') // Load config file with access keys and tokens
+
+var T = new Twit(config);
 
 console.log('The bot is starting');
 
-var T = new Twit(config);
+var params = {};
+
+var stream = T.stream('statuses/filter', {follow: '15934386'}); // Currently follows me
+
+var count = 0;
+
+stream.on('tweet', function (tweet) {
+  console.log("They tweeted!");
+});
+
+// function tweetTrump(){
+//   console.log('Tweeting Trump!');
+//   var tweet = 'STFU DONNY';
+//   T.post('statuses/update', tweet, tweeted);
+// };
